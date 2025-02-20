13261f97ba23563830d4579a6a150990e6b8091b
bEN <benhernowitz@gmail.com>
2025-02-20 11:18:14 -0500

    added table skeleton

--- capstoneBE/server/db.js
+++ capstoneBE/server/db.js
@@ -1,4 +1,9 @@
 const pg = require("pg");
 const client = new pg.Client();
 
+const createTables = async () => {
+  const SQL = `cd `;
+  await client.query(SQL);
+};
+
 module.exports = { client };
