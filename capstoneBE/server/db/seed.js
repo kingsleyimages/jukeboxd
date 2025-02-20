e91bdbd2f2c7775deb8399840f75e454d65c86ab
Scott Kingsley <scott.kingsley@kingsleyimages.comgh auth logingit config --global user.email scott.kingsley@kingsleyimages.co>
2025-02-20 11:01:19 -0700

    table structure

---
+++ capstoneBE/server/db/seed.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const { client, createTables } = require('./index.js');
+
+const seed = async () => {
+  try {
+    await client.connect();
+    console.log('connected to database');
+    await createTables();
+    // seed your database here!
+  } catch (error) {
+    console.error('error creating tables');
+    throw error;
+  }
+};
+seed();
