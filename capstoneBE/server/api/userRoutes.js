8092a363fc34a9fce8974c2c65d7af8cf664148a
bEN <benhernowitz@gmail.com>
2025-02-21 10:35:51 -0500

    fixed register route

--- capstoneBE/server/api/userRoutes.js
+++ capstoneBE/server/api/userRoutes.js
@@ -1,19 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const { createUser } = require("../db/user.js");
 
-const { createUser } = require('../db/user.js');
-
-router.post('/', async (req, res, next) => {
-  console.log(req.body)
+router.post("/", async (req, res, next) => {
+  console.log(req.body);
   try {
-    const review = await createUser(
+    const response = await createUser(
       req.body.username,
       req.body.password,
       req.body.role
     );
 
-    res.status(201).send(review);
+    res.status(201).send(response);
   } catch (error) {
     next(error);
   }
