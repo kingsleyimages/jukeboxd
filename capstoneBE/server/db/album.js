2c4bff0ab2747f23a49b24379ab2c345b3d96b1c
bEN <benhernowitz@gmail.com>
2025-02-24 11:44:01 -0500

    added spotify_id to fetchalbumbyid query

--- capstoneBE/server/db/album.js
+++ capstoneBE/server/db/album.js
@@ -1,5 +1,5 @@
-const { client } = require('./index');
-const uuid = require('uuid');
+const { client } = require("./index");
+const uuid = require("uuid");
 
 const createAlbum = async (spotify_id, name, artist, image, spotifyUrl) => {
   try {
@@ -35,7 +35,7 @@ const fetchAlbums = async () => {
 
 const fetchAlbumById = async (id) => {
   const SQL = `
-    SELECT * FROM albums WHERE id = $1
+    SELECT * FROM albums WHERE spotify_id = $1
   `;
   const response = await client.query(SQL, [id]);
   return response.rows[0];
