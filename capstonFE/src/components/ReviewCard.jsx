36b758300a28a635c8a1a12d14ed5ec5049f3fd0
bEN <benhernowitz@gmail.com>
2025-02-26 14:12:17 -0500

    added edit review feature

--- capstonFE/src/components/ReviewCard.jsx
+++ capstonFE/src/components/ReviewCard.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import styles from '../css/AlbumDetails.module.css';
+import React from "react";
+import styles from "../css/AlbumDetails.module.css";
 
-const ReviewCard = ({ review }) => (
+const ReviewCard = ({ review, userId, onEditClick }) => (
   <div className={styles.reviewCard}>
     <h3 className={styles.headline}>{review.headline}</h3>
     <p className={styles.author}>By: {review.username}</p>
     <p className={styles.review}>{review.review}</p>
     <p className={styles.rating}>Rating: {review.rating} / 5</p>
-    <p className={styles.favorite}>{review.favorite ? 'Favorite: ❤️' : ''}</p>
+    <p className={styles.favorite}>{review.favorite ? "Favorite: ❤️" : ""}</p>
+    {userId === review.user_id && (
+      <button className={styles.editButton} onClick={() => onEditClick(review)}>
+        Edit Review
+      </button>
+    )}
   </div>
 );
 
