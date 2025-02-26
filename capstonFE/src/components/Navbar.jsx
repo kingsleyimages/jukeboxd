2bb3e11fe7247a71d272cfb002dff8952f80e960
Kathleen Hamrick <kchamrick@gmail.com>
2025-02-26 12:45:10 -0500

    navbar updates

--- capstonFE/src/components/Navbar.jsx
+++ capstonFE/src/components/Navbar.jsx
@@ -49,6 +49,8 @@ function Navbar() {
           <>
             {/* Navigation for logged-in users */}
             <Link to="/">Home</Link>
+            <Link to="/discover">Discover</Link>
+            <Link to="/search">Search</Link>
             <Link to="/account">Account</Link>
             <button onClick={onLogout} className="logout-button">
               Log out
