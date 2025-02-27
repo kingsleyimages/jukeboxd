8a17689774bfdee740b1b60d8897d7de07d9fd3d
Scott Kingsley <scott.kingsley@kingsleyimages.comgh auth logingit config --global user.email scott.kingsley@kingsleyimages.co>
2025-02-27 12:21:28 -0700

    port

--- capstoneBE/app.js
+++ capstoneBE/app.js
@@ -2,7 +2,7 @@ require('dotenv').config({ path: '../.env' });
 const { client } = require('./server/db/index.js');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 const cors = require('cors');
 
