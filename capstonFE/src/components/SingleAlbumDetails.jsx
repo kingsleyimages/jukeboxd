36445052d515ca7126bd00214276612968077566
Nathanael Santiago <nsantiago64@icloud.com>
2025-02-28 12:38:12 -0500

    added listened to button on singlepage

--- capstonFE/src/components/SingleAlbumDetails.jsx
+++ capstonFE/src/components/SingleAlbumDetails.jsx
@@ -4,237 +4,266 @@ import ReviewCard from "./ReviewCard";
 import styles from "../css/AlbumDetails.module.css";
 
 const AlbumDetails = ({ token }) => {
-  const { albumId } = useParams();
-  const [album, setAlbum] = useState(null);
-  const [reviews, setReviews] = useState([]);
-  const [userId, setUserId] = useState(null);
-  const [editingReview, setEditingReview] = useState(null);
-  const [formData, setFormData] = useState({
-    headline: "",
-    review: "",
-    rating: 1,
-    favorite: false,
-  });
-  const API_BASE_URL =
-    import.meta.env.VITE_API_BASE_URL_PROD ||
-    import.meta.env.VITE_API_BASE_URL_DEV;
+	const { albumId } = useParams();
+	const [album, setAlbum] = useState(null);
+	const [reviews, setReviews] = useState([]);
+	const [userId, setUserId] = useState(null);
+	const [editingReview, setEditingReview] = useState(null);
+	const [loading, setLoading] = useState(true);
+	const [formData, setFormData] = useState({
+		headline: "",
+		review: "",
+		rating: 1,
+		favorite: false,
+	});
 
-  useEffect(() => {
-    const fetchAlbumDetails = async () => {
-      try {
-        const response = await fetch(`${API_BASE_URL}/api/albums/${albumId}`);
-        const data = await response.json();
-        setAlbum(data);
-        setReviews(data.reviews || []);
-      } catch (error) {
-        console.error("Error fetching album details:", error.message);
-      }
-    };
+	const API_BASE_URL =
+		import.meta.env.VITE_API_BASE_URL_PROD ||
+		import.meta.env.VITE_API_BASE_URL_DEV;
 
-    fetchAlbumDetails();
+	useEffect(() => {
+		const fetchAlbumDetails = async () => {
+			try {
+				const response = await fetch(
+					`${API_BASE_URL}/api/albums/${albumId}`
+				);
+				const data = await response.json();
+				setAlbum(data);
+				setReviews(data.reviews || []);
+			} catch (error) {
+				console.error("Error fetching album details:", error.message);
+			} finally {
+				setLoading(false);
+			}
+		};
 
-    if (token) {
-      try {
-        const payload = JSON.parse(atob(token.split(".")[1]));
-        setUserId(payload.id);
-      } catch (error) {
-        console.error("Invalid token format", error.message);
-      }
-    }
-  }, [albumId, token]);
+		fetchAlbumDetails();
 
-  const handleSubmitReview = async (e) => {
-    e.preventDefault();
+		if (token) {
+			try {
+				const payload = JSON.parse(atob(token.split(".")[1]));
+				if (payload && payload.id) {
+					setUserId(payload.id);
+				}
+			} catch (error) {
+				console.error("Invalid token format:", error.message);
+			}
+		}
+	}, [albumId, token]);
 
-    if (!token) {
-      alert("You need to be logged in to leave a review");
-      return;
-    }
+	const markAsListened = async () => {
+		const storedToken = localStorage.getItem("token");
+		if (!storedToken) {
+			console.error("No token found, user might not be logged in.");
+			return;
+		}
 
-    try {
-      const url = editingReview
-        ? `${API_BASE_URL}/api/reviews/${editingReview.id}/update`
-        : `${API_BASE_URL}/api/reviews/album/${albumId}/create`;
+		try {
+			const response = await fetch(
+				`${API_BASE_URL}/api/albums/listened`,
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: `Bearer ${storedToken}`,
+					},
+					body: JSON.stringify({ album_id: albumId }),
+				}
+			);
 
-      const method = editingReview ? "PUT" : "POST";
+			if (!response.ok) throw new Error("Failed to update album");
 
-      const response = await fetch(url, {
-        method,
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify(formData),
-      });
+			setAlbum((prevAlbum) => ({ ...prevAlbum, listened: true }));
+		} catch (error) {
+			console.error(
+				"Error marking album as listened:",
+				error.message
+			);
+		}
+	};
 
-      if (response.ok) {
-        const updatedReview = await response.json();
-        setReviews((prevReviews) =>
-          prevReviews.map((review) =>
-            review.id === updatedReview.id ? updatedReview : review
-          )
-        );
-        setEditingReview(null);
-        setFormData({ headline: "", review: "", rating: 1, favorite: false });
-      } else {
-        console.error("Failed to save review");
-      }
-    } catch (err) {
-      console.error("Error saving review:", err.message);
-    }
-  };
+	const handleSubmitReview = async (e) => {
+		e.preventDefault();
+		if (!token) {
+			alert("You need to be logged in to leave a review");
+			return;
+		}
 
-  const handleInputChange = (e) => {
-    const { name, value, type, checked } = e.target;
-    setFormData((prev) => ({
-      ...prev,
-      [name]: type === "checkbox" ? checked : value,
-    }));
-  };
+		try {
+			const url = editingReview
+				? `${API_BASE_URL}/api/reviews/${editingReview.id}/update`
+				: `${API_BASE_URL}/api/reviews/album/${albumId}/create`;
 
-  const handleCancelEdit = () => {
-    setEditingReview(null);
-    setFormData({ headline: "", review: "", rating: 1, favorite: false });
-  };
+			const method = editingReview ? "PUT" : "POST";
+			const response = await fetch(url, {
+				method,
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${token}`,
+				},
+				body: JSON.stringify(formData),
+			});
 
-  const handleEditClick = (review) => {
-    setEditingReview(review);
-    setFormData({
-      headline: review.headline,
-      review: review.review,
-      rating: review.rating,
-      favorite: review.favorite,
-    });
-    console.log("Edit review:", review);
-  };
+			if (response.ok) {
+				const updatedReview = await response.json();
+				setReviews((prevReviews) =>
+					editingReview
+						? prevReviews.map((review) =>
+								review.id === updatedReview.id
+									? updatedReview
+									: review
+						  )
+						: [...prevReviews, updatedReview]
+				);
+				setEditingReview(null);
+				setFormData({
+					headline: "",
+					review: "",
+					rating: 1,
+					favorite: false,
+				});
+			} else {
+				console.error("Failed to save review");
+			}
+		} catch (err) {
+			console.error("Error saving review:", err.message);
+		}
+	};
 
-  if (!album) return <div>Loading...</div>;
+	const handleDeleteClick = async (review) => {
+		if (
+			!window.confirm("Are you sure you want to delete this review?")
+		)
+			return;
+		try {
+			const response = await fetch(
+				`${API_BASE_URL}/api/reviews/${review.id}/delete`,
+				{
+					method: "DELETE",
+					headers: {
+						Authorization: `Bearer ${token}`,
+						"Content-Type": "application/json",
+					},
+				}
+			);
 
-  const handleDeleteClick = async (review) => {
-    const confirmDelete = window.confirm(
-      "Are you sure you want to delete this review?"
-    );
-    if (!confirmDelete) return;
+			if (response.ok) {
+				setReviews((prevReviews) =>
+					prevReviews.filter((r) => r.id !== review.id)
+				);
+			} else {
+				alert("Failed to delete the review. Please try again.");
+			}
+		} catch (error) {
+			console.error("Error deleting review:", error);
+			alert("An error occurred while deleting the review.");
+		}
+	};
 
-    try {
-      const response = await fetch(
-        `${API_BASE_URL}/api/reviews/${review.id}/delete`,
-        {
-          method: "DELETE",
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-        }
-      );
+	if (loading) return <div>Loading...</div>;
+	if (!album) return <div>Album not found.</div>;
 
-      if (response.ok) {
-        alert("Review deleted successfully!");
-      } else {
-        alert("Failed to delete the review. Please try again.");
-      }
-    } catch (error) {
-      console.error("Error deleting review:", error);
-      alert("An error occurred while deleting the review.");
-    }
-  };
+	const averageRating =
+		reviews.length > 0
+			? (
+					reviews.reduce((acc, review) => acc + review.rating, 0) /
+					reviews.length
+			  ).toFixed(1)
+			: null;
 
-  const averageRating =
-    reviews.length > 0
-      ? (
-          reviews.reduce((acc, review) => acc + review.rating, 0) /
-          reviews.length
-        ).toFixed(1)
-      : null;
-
-  return (
-    <div>
-      <div className={styles.topContainer}>
-        <div className={styles.albumWrapper}>
-          <h1 className={styles.title}>{album.name}</h1>
-          <p className={styles.artist}>{album.artist}</p>
-          <img className={styles.img} src={album.image} alt={album.name} />
-          {averageRating && <p>Average Rating: {averageRating} /5</p>}
-          {album?.tracks?.length > 0 ? (
-            album.tracks.map((track) => (
-              <p key={track.id}>
-                {track.track_number}. {track.title}
-              </p>
-            ))
-          ) : (
-            <p>No tracks for this album</p>
-          )}
-        </div>
-        <div className={styles.formContainer}>
-          {token && (
-            <form onSubmit={handleSubmitReview}>
-              <h2>{editingReview ? "Edit Review" : "Leave a Review"}</h2>
-              <input
-                type="text"
-                name="headline"
-                placeholder="Headline"
-                value={formData.headline}
-                onChange={handleInputChange}
-                required
-              />
-              <textarea
-                name="review"
-                placeholder="Write your review..."
-                value={formData.review}
-                onChange={handleInputChange}
-                required
-              />
-              <input
-                name="rating"
-                type="number"
-                min="1"
-                max="5"
-                placeholder="Rating (1-5)"
-                value={formData.rating}
-                onChange={handleInputChange}
-                required
-              />
-              <label>
-                <input
-                  name="favorite"
-                  type="checkbox"
-                  checked={formData.favorite}
-                  onChange={handleInputChange}
-                />
-                Mark as Favorite
-              </label>
-              <button type="submit">
-                {editingReview ? "Update Review" : "Submit Review"}
-              </button>
-              {editingReview && (
-                <button type="button" onClick={handleCancelEdit}>
-                  Cancel
-                </button>
-              )}
-            </form>
-          )}
-        </div>
-      </div>
-      <div className={styles.reviewContainer}>
-        <h2 className={styles.reviewTitle}>Reviews:</h2>
-        <div className={styles.reviewWrapper}>
-          {reviews.length > 0 ? (
-            reviews.map((review) => (
-              <ReviewCard
-                key={review.id}
-                review={review}
-                userId={userId}
-                onEditClick={handleEditClick}
-                onDeleteClick={handleDeleteClick}
-              />
-            ))
-          ) : (
-            <p>No Reviews for this album</p>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className={styles.topContainer}>
+				<div className={styles.albumWrapper}>
+					<h1 className={styles.title}>{album.name}</h1>
+					<p className={styles.artist}>{album.artist}</p>
+					<img
+						className={styles.img}
+						src={album.image}
+						alt={album.name}
+					/>
+					{averageRating && <p>Average Rating: {averageRating} /5</p>}
+					{album?.tracks?.length > 0 ? (
+						album.tracks.map((track) => (
+							<p key={track.id}>
+								{track.track_number}. {track.title}
+							</p>
+						))
+					) : (
+						<p>No tracks for this album</p>
+					)}
+				</div>
+				{token && (
+					<form
+						onSubmit={handleSubmitReview}
+						className={styles.formContainer}
+					>
+						<h2>
+							{editingReview ? "Edit Review" : "Leave a Review"}
+						</h2>
+						<input
+							type="text"
+							name="headline"
+							placeholder="Headline"
+							value={formData.headline}
+							onChange={(e) =>
+								setFormData({ ...formData, headline: e.target.value })
+							}
+							required
+						/>
+						<textarea
+							name="review"
+							placeholder="Write your review..."
+							value={formData.review}
+							onChange={(e) =>
+								setFormData({ ...formData, review: e.target.value })
+							}
+							required
+						/>
+						<input
+							type="number"
+							name="rating"
+							min="1"
+							max="5"
+							value={formData.rating}
+							onChange={(e) =>
+								setFormData({
+									...formData,
+									rating: Number(e.target.value),
+								})
+							}
+							required
+						/>
+						<button type="submit">
+							{editingReview ? "Update Review" : "Submit Review"}
+						</button>
+					</form>
+				)}
+			</div>
+			<div className={styles.reviewContainer}>
+				<h2 className={styles.reviewTitle}>Reviews:</h2>
+				{reviews.length > 0 ? (
+					reviews.map((review) => (
+						<ReviewCard
+							key={review.id}
+							review={review}
+							userId={userId}
+							onDeleteClick={handleDeleteClick}
+						/>
+					))
+				) : (
+					<p>No Reviews for this album</p>
+				)}
+				<button
+					className={styles.button}
+					onClick={markAsListened}
+					disabled={album.listened}
+				>
+					{album.listened ? "Already Listened" : "Mark as Listened"}
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default AlbumDetails;
