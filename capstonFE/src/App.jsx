e94b0f62a72c66f056d0ea39500820651d142626
Nathanael Santiago <nsantiago64@icloud.com>
2025-02-28 12:07:12 -0500

    added listened to functionality in the search page

--- capstonFE/src/App.jsx
+++ capstonFE/src/App.jsx
@@ -1,57 +1,60 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Discover from './components/Discover';
-import Navbar from './components/Navbar';
-import ProtectedRoute from './components/ProtectedRoute';
-import Me from './components/Me';
-import Home from './components/Home';
-import Mixtapes from './components/Mixtapes';
-import Search from './components/Search';
-import Register from './components/Register';
-import Login from './components/Login';
-import Callback from './components/Callback';
-import SingleAlbumDetails from './components/SingleAlbumDetails';
-import Friends from './components/Friends';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Discover from "./components/Discover";
+import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Me from "./components/Me";
+import Home from "./components/Home";
+import Mixtapes from "./components/Mixtapes";
+import Search from "./components/Search";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Callback from "./components/Callback";
+import SingleAlbumDetails from "./components/SingleAlbumDetails";
+import Friends from "./components/Friends";
 // import Admin from './components/Admin'; // Import the Admin component
-import AdminDashboard from './components/home-components/AdminDashBoard'; // Import the AdminDashboard component
+import AdminDashboard from "./components/home-components/AdminDashBoard"; // Import the AdminDashboard component
 
 function App() {
-  const [token, setToken] = useState('');
+	const [token, setToken] = useState("");
 
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    setToken(token);
-    console.log('App loaded, userId:', token);
-  }, []);
+	useEffect(() => {
+		const token = localStorage.getItem("token");
+		setToken(token);
+		console.log("App loaded, userId:", token);
+	}, []);
 
-  return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route element={<ProtectedRoute />}>
-            <Route path="/account" element={<Me />} />
-            {/* <Route path="/admin" element={<Admin />} />{" "}
+	return (
+		<>
+			<BrowserRouter>
+				<Navbar />
+				<Routes>
+					<Route element={<ProtectedRoute />}>
+						<Route path="/account" element={<Me />} />
+						{/* <Route path="/admin" element={<Admin />} />{" "}
             Add the Admin route inside ProtectedRoute */}
-            <Route path="/admin/dashboard" element={<AdminDashboard />} />{' '}
-            {/* Add the AdminDashboard route inside ProtectedRoute */}
-          </Route>
-          <Route path="/callback" element={<Callback />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/mixtapes" element={<Mixtapes />} />
-          <Route path="/discover" element={<Discover />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/friends" element={<Friends />} />
-          <Route
-            path="/album/:albumId"
-            element={<SingleAlbumDetails token={token} />}
-          />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+						<Route
+							path="/admin/dashboard"
+							element={<AdminDashboard />}
+						/>{" "}
+						{/* Add the AdminDashboard route inside ProtectedRoute */}
+					</Route>
+					<Route path="/callback" element={<Callback />} />
+					<Route path="/" element={<Home />} />
+					<Route path="/mixtapes" element={<Mixtapes />} />
+					<Route path="/discover" element={<Discover />} />
+					<Route path="/search" element={<Search />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="/friends" element={<Friends />} />
+					<Route
+						path="/album/:albumId"
+						element={<SingleAlbumDetails token={token} />}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
--- capstonFE/src/components/Search.jsx
+++ capstonFE/src/components/Search.jsx
@@ -1,254 +1,280 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../App.css";
 import styles from "../css/Search.module.css";
 import {
-  FormControl,
-  InputGroup,
-  Container,
-  Button,
-  Card,
-  Row,
+	FormControl,
+	InputGroup,
+	Container,
+	Button,
+	Card,
+	Row,
 } from "react-bootstrap";
-import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const clientId = import.meta.env.VITE_CLIENT_ID;
 const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
+const API_BASE_URL =
+	import.meta.env.VITE_API_BASE_URL_PROD ||
+	import.meta.env.VITE_API_BASE_URL_DEV;
 
 function App() {
-  const [searchInput, setSearchInput] = useState("");
-  const [accessToken, setAccessToken] = useState("");
-  const [albums, setAlbums] = useState([]);
-  const navigate = useNavigate();
-  const API_BASE_URL =
-    import.meta.env.VITE_API_BASE_URL_PROD ||
-    import.meta.env.VITE_API_BASE_URL_DEV;
+	const [searchInput, setSearchInput] = useState("");
+	const [accessToken, setAccessToken] = useState("");
+	const [albums, setAlbums] = useState([]);
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    let authParams = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body:
-        "grant_type=client_credentials&client_id=" +
-        clientId +
-        "&client_secret=" +
-        clientSecret,
-    };
+	useEffect(() => {
+		let authParams = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+			},
+			body:
+				"grant_type=client_credentials&client_id=" +
+				clientId +
+				"&client_secret=" +
+				clientSecret,
+		};
 
-    fetch("https://accounts.spotify.com/api/token", authParams)
-      .then((result) => result.json())
-      .then((data) => {
-        setAccessToken(data.access_token);
-        console.log(data);
-      });
-  }, []);
+		fetch("https://accounts.spotify.com/api/token", authParams)
+			.then((result) => result.json())
+			.then((data) => {
+				setAccessToken(data.access_token);
+			});
+	}, []);
 
-  async function addReview(e) {
-    //needs to be fixed!!
-    // e.preventDefault();
-    // e.selectTarget;
-    // const response = await fetch("", { method: "POST" });
-  }
+	const markAsListened = async (albumId) => {
+		const token = localStorage.getItem("token"); // Retrieve JWT from localStorage
 
-  async function search() {
-    let artistParams = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + accessToken,
-      },
-    };
+		if (!token) {
+			console.error("No token found, user might not be logged in.");
+			return;
+		}
 
-    // Get Artist
-    const artistID = await fetch(
-      "https://api.spotify.com/v1/search?q=" + searchInput + "&type=artist",
-      artistParams
-    )
-      .then((result) => result.json())
-      .then((data) => {
-        return data.artists.items[0].id;
-      });
+		try {
+			const response = await fetch(
+				`${API_BASE_URL}/api/albums/listened`,
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: `Bearer ${token}`, // Attach token for authentication
+					},
+					body: JSON.stringify({ album_id: albumId }),
+				}
+			);
 
-    // Get Artist Albums
-    await fetch(
-      "https://api.spotify.com/v1/artists/" +
-        artistID +
-        "/albums?include_groups=album&market=US&limit=50",
-      artistParams
-    )
-      .then((result) => result.json())
-      .then((data) => {
-        setAlbums(data.items);
-      });
-    // Get Artist Albums
-    await fetch(
-      "https://api.spotify.com/v1/artists/" +
-        artistID +
-        "/albums?include_groups=album&market=US&limit=50",
-      artistParams
-    )
-      .then((result) => result.json())
-      .then((data) => {
-        setAlbums(data.items);
-      });
-  }
+			if (!response.ok) throw new Error("Failed to update album");
 
-  const handleViewDetails = async (albumId) => {
-    try {
-      // Check if album exists in local database
-      const localResponse = await fetch(
-        `${API_BASE_URL}/api/albums/${albumId}`
-      );
+			const data = await response.json();
+			console.log("Album marked as listened:", data);
 
-      if (!localResponse.ok) {
-        console.error(
-          "local album fetch failed with status",
-          localResponse.status
-        );
-      }
+			// Update UI
+			setAlbums((prevAlbums) =>
+				prevAlbums.map((album) =>
+					album.id === albumId ? { ...album, listened: true } : album
+				)
+			);
+		} catch (error) {
+			console.error(
+				"Error marking album as listened:",
+				error.message
+			);
+		}
+	};
 
-      const localResult = await localResponse.json().catch(() => null);
-      console.log("local database result:", localResult);
+	async function search() {
+		let artistParams = {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: "Bearer " + accessToken,
+			},
+		};
 
-      // If album exists, navigate to that page
-      if (localResult?.id) {
-        navigate(`/album/${albumId}`);
-        return;
-      }
+		const artistID = await fetch(
+			`https://api.spotify.com/v1/search?q=${searchInput}&type=artist`,
+			artistParams
+		)
+			.then((result) => result.json())
+			.then((data) => data.artists.items[0]?.id);
 
-      console.log("album not found locally, fetching from spotify");
+		if (!artistID) {
+			console.error("No artist found");
+			return;
+		}
 
-      // If album doesn't exist, fetch it from Spotify
-      const spotifyResponse = await fetch(
-        `https://api.spotify.com/v1/albums/${albumId}`,
-        {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-            "Content-Type": "application/json",
-          },
-        }
-      );
+		await fetch(
+			`https://api.spotify.com/v1/artists/${artistID}/albums?include_groups=album&market=US&limit=50`,
+			artistParams
+		)
+			.then((result) => result.json())
+			.then((data) => {
+				setAlbums(data.items);
+			});
+	}
 
-      // Check for Spotify API errors
-      if (!spotifyResponse.ok) {
-        console.error(
-          "spotify fetch failed with status",
-          spotifyResponse.status
-        );
-        return;
-      }
+	const handleViewDetails = async (albumId) => {
+		try {
+			// Check if album exists in local database
+			const localResponse = await fetch(
+				`${API_BASE_URL}/api/albums/${albumId}`
+			);
 
-      const spotifyResult = await spotifyResponse.json().catch(() => null);
+			if (!localResponse.ok) {
+				console.error(
+					"Local album fetch failed with status",
+					localResponse.status
+				);
+			}
 
-      if (!spotifyResult) {
-        console.error("failed to parse Spotify API response as JSON");
-        return;
-      }
-      console.log("spotify api result:", spotifyResult);
+			const localResult = await localResponse
+				.json()
+				.catch(() => null);
+			console.log("Local database result:", localResult);
 
-      //prepare album data for local database
-      const albumData = {
-        name: spotifyResult.name,
-        artist: spotifyResult.artists[0]?.name || "unknown artist",
-        image: spotifyResult.images[0]?.url || "",
-        spotify_id: spotifyResult.id,
-        spotifyUrl: spotifyResult.external_urls?.spotify,
-        tracks: spotifyResult.tracks.items.map((track) => ({
-          title: track.name,
-          spotify_id: track.id,
-          track_number: track.track_number,
-        })),
-      };
+			// If album exists, navigate to that page
+			if (localResult?.id) {
+				navigate(`/album/${albumId}`);
+				return;
+			}
 
-      // Save the album to local database
+			console.log("Album not found locally, fetching from Spotify");
 
-      // await fetch(`https://jukeboxd-znlr.onrender.com/albums`, {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      //   body: JSON.stringify(spotifyResult),
-      // });
-      const saveResponse = await fetch(`${API_BASE_URL}/api/albums/create`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(albumData),
-      });
+			// If album doesn't exist, fetch it from Spotify
+			const spotifyResponse = await fetch(
+				`https://api.spotify.com/v1/albums/${albumId}`,
+				{
+					method: "GET",
+					headers: {
+						Authorization: `Bearer ${accessToken}`,
+						"Content-Type": "application/json",
+					},
+				}
+			);
 
-      if (!saveResponse.ok) {
-        throw new Error("failed to save album to local db");
-      }
-      console.log("album saved to local db");
+			// Check for Spotify API errors
+			if (!spotifyResponse.ok) {
+				console.error(
+					"Spotify fetch failed with status",
+					spotifyResponse.status
+				);
+				return;
+			}
 
-      // Navigate to the new album page
-      navigate(`/album/${albumId}`);
-    } catch (error) {
-      console.error("Error handling album details:", error);
-    }
-  };
+			const spotifyResult = await spotifyResponse
+				.json()
+				.catch(() => null);
 
-  return (
-    <>
-      <Container className={styles.searchContainer}>
-        <InputGroup className={styles.inner}>
-          <FormControl
-            placeholder="Search For Artist"
-            type="input"
-            aria-label="Search for an Artist"
-            onKeyDown={(event) => {
-              if (event.key === "Enter") {
-                search();
-              }
-            }}
-            onChange={(event) => setSearchInput(event.target.value)}
-            className={styles.search}
-          />
-          <Button className={styles.button} onClick={search}>
-            Search
-          </Button>
-          {/* <Button className={styles.button} onClick={addReview}>
-            Add a Review
-          </Button> */}
-        </InputGroup>
-      </Container>
+			if (!spotifyResult) {
+				console.error("Failed to parse Spotify API response as JSON");
+				return;
+			}
+			console.log("Spotify API result:", spotifyResult);
 
-      <Container>
-        <Row className={styles.row}>
-          {albums.map((album) => {
-            return (
-              <Card key={album.id} className={styles.card}>
-                <Card.Img src={album.images[0].url} className={styles.img} />
-                <Card.Body>
-                  <Card.Title className={styles.title}>{album.name}</Card.Title>
-                  <Card.Text className={styles.release}>
-                    Release Date: <br /> {album.release_date}
-                  </Card.Text>
-                  <Button
-                    target="_blank"
-                    href={album.external_urls.spotify}
-                    className={styles.button}
-                  >
-                    Album Link
-                  </Button>
-                  <Button
-                    className={styles.button}
-                    onClick={() => handleViewDetails(album.id)}
-                  >
-                    View Details
-                  </Button>
-                </Card.Body>
-              </Card>
-            );
-          })}
-        </Row>
-      </Container>
-    </>
-  );
+			// Prepare album data for local database
+			const albumData = {
+				name: spotifyResult.name,
+				artist: spotifyResult.artists[0]?.name || "Unknown Artist",
+				image: spotifyResult.images[0]?.url || "",
+				spotify_id: spotifyResult.id,
+				spotifyUrl: spotifyResult.external_urls?.spotify,
+				tracks: spotifyResult.tracks.items.map((track) => ({
+					title: track.name,
+					spotify_id: track.id,
+					track_number: track.track_number,
+				})),
+			};
+
+			// Save the album to local database
+			const saveResponse = await fetch(
+				`${API_BASE_URL}/api/albums/create`,
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify(albumData),
+				}
+			);
+
+			if (!saveResponse.ok) {
+				throw new Error("Failed to save album to local database");
+			}
+			console.log("Album saved to local database");
+
+			// Navigate to the new album page
+			navigate(`/album/${albumId}`);
+		} catch (error) {
+			console.error("Error handling album details:", error);
+		}
+	};
+
+	return (
+		<>
+			<Container className={styles.searchContainer}>
+				<InputGroup className={styles.inner}>
+					<FormControl
+						placeholder="Search For Artist"
+						type="input"
+						onKeyDown={(event) => {
+							if (event.key === "Enter") {
+								search();
+							}
+						}}
+						onChange={(event) => setSearchInput(event.target.value)}
+						className={styles.search}
+					/>
+					<Button className={styles.button} onClick={search}>
+						Search
+					</Button>
+				</InputGroup>
+			</Container>
+
+			<Container>
+				<Row className={styles.row}>
+					{albums.map((album) => (
+						<Card key={album.id} className={styles.card}>
+							<Card.Img
+								src={album.images[0]?.url || ""}
+								className={styles.img}
+							/>
+							<Card.Body>
+								<Card.Title className={styles.title}>
+									{album.name}
+								</Card.Title>
+								<Card.Text className={styles.release}>
+									Release Date: <br /> {album.release_date}
+								</Card.Text>
+								<Button
+									target="_blank"
+									href={album.external_urls.spotify}
+									className={styles.button}
+								>
+									Album Link
+								</Button>
+								<Button
+									className={styles.button}
+									onClick={() => markAsListened(album.id)}
+									disabled={album.listened}
+								>
+									{album.listened
+										? "Already Listened"
+										: "Mark as Listened"}
+								</Button>
+								<Button
+									className={styles.button}
+									onClick={() => handleViewDetails(album.id)}
+								>
+									View Details
+								</Button>
+							</Card.Body>
+						</Card>
+					))}
+				</Row>
+			</Container>
+		</>
+	);
 }
 
 export default App;
