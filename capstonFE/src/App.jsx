41ebedbe29a123c50810efecf70d30b5d48bdb2d
Scott Kingsley <scott.kingsley@kingsleyimages.comgh auth logingit config --global user.email scott.kingsley@kingsleyimages.co>
2025-03-04 11:31:51 -0700

    review page
    fix`:wq
    :WQ

--- capstonFE/src/App.jsx
+++ capstonFE/src/App.jsx
@@ -1,34 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Discover from './components/Discover';
-import Navbar from './components/Navbar';
-import ProtectedRoute from './components/ProtectedRoute';
-import Me from './components/Me';
-import Home from './components/Home';
-import Mixtapes from './components/Mixtapes';
-import Search from './components/Search';
-import Register from './components/Register';
-import Login from './components/Login';
-import Callback from './components/Callback';
-import SingleAlbumDetails from './components/SingleAlbumDetails';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Discover from "./components/Discover";
+import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Me from "./components/Me";
+import Home from "./components/Home";
+import Mixtapes from "./components/Mixtapes";
+import Search from "./components/Search";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Callback from "./components/Callback";
+import AllReviews from "./components/AllReviews";
+import SingleAlbumDetails from "./components/SingleAlbumDetails";
 
-import AdminDashboard from './components/home-components/AdminDashBoard';
-import UserDetails from './components/admin-components/UserDetails';
-import ViewAllUsers from './components/admin-components/ViewAllUsers';
-import ReviewedAlbums from './components/admin-components/ReviewedAlbums';
-import UserReviewsPage from './components/admin-components/UserReviewsPage';
-import UserCommentsPage from './components/admin-components/UserCommentsPage';
-import UserModify from './components/admin-components/modify-components/UsersModify';
-import ModifyReview from './components/admin-components/modify-components/ModifyReview';
-import ModifyComment from './components/admin-components/modify-components/ModifyComment';
+import AdminDashboard from "./components/home-components/AdminDashBoard";
+import UserDetails from "./components/admin-components/UserDetails";
+import ViewAllUsers from "./components/admin-components/ViewAllUsers";
+import ReviewedAlbums from "./components/admin-components/ReviewedAlbums";
+import UserReviewsPage from "./components/admin-components/UserReviewsPage";
+import UserCommentsPage from "./components/admin-components/UserCommentsPage";
+import UserModify from "./components/admin-components/modify-components/UsersModify";
+import ModifyReview from "./components/admin-components/modify-components/ModifyReview";
+import ModifyComment from "./components/admin-components/modify-components/ModifyComment";
 
 function App() {
-  const [token, setToken] = useState('');
+  const [token, setToken] = useState("");
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     setToken(token);
-    console.log('App loaded, userId:', token);
+    console.log("App loaded, userId:", token);
   }, []);
 
   return (
@@ -39,8 +40,7 @@ function App() {
           <Route element={<ProtectedRoute />}>
             <Route path="/account" element={<Me />} />
             <Route path="/admin/dashboard" element={<AdminDashboard />} />
-       
-       
+
             <Route path="/admin/dashboard/users" element={<ViewAllUsers />} />
             <Route
               path="/admin/dashboard/reviewed-albums"
@@ -68,6 +68,7 @@ function App() {
           <Route path="/callback" element={<Callback />} />
           <Route path="/" element={<Home />} />
           <Route path="/mixtapes" element={<Mixtapes />} />
+          <Route path="/reviews" element={<AllReviews />} />
           <Route path="/discover" element={<Discover />} />
           <Route path="/search" element={<Search />} />
           <Route path="/login" element={<Login />} />
