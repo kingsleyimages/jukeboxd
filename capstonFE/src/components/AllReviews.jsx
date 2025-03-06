7e4c1d4bef2a374147ee967159b5faa60f290a39
Scott Kingsley <scott.kingsley@kingsleyimages.comgh auth logingit config --global user.email scott.kingsley@kingsleyimages.co>
2025-03-06 11:18:26 -0700

    edits to review page

--- capstonFE/src/components/AllReviews.jsx
+++ capstonFE/src/components/AllReviews.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import styles from "../css/AlbumDetails.module.css";
 import axios from "axios";
 
@@ -24,27 +25,38 @@ const AllReviews = () => {
 
   return (
     <div className={styles.allReviews}>
-      <h2>All Reviews</h2>
-      {reviews.length > 0 ? (
-        reviews.map((review) => (
-          <div key={review.id} className={styles.reviewCard}>
-            <h3 className={styles.headline}>{review.album_name}</h3>
-            <p className={styles.headline}>{review.album_name}</p>
-            <img src={review.album_image} alt={review.album_name} />
-            <h3 className={styles.headline}>{review.headline}</h3>
-            <p className={styles.author}>By: {review.username}</p>
-            <p className={styles.review}>Reviewed: {review.updated_at}</p>
-            <p className={styles.review}>{review.review}</p>
-            <p className={styles.rating}>Rating: {review.rating} / 5</p>
-            <br />
-            <p className={styles.favorite}>
-              {review.favorite ? "Favorite: ❤️" : ""}
-            </p>
-          </div>
-        ))
-      ) : (
-        <p>No reviews yet.</p>
-      )}
+      <h1 className={styles.allReviewHeader}>Last 20 Reviews</h1>
+      <div className={styles.reviewPageWrapper}>
+        {reviews.length > 0 ? (
+          reviews.map((review) => (
+            <div key={review.id} className={styles.reviewPageCard}>
+              <div className={styles.albumContainer}>
+                <h3 className={styles.headline}>{review.album_name}</h3>
+                <p className={styles.review}>{review.album_artist}</p>
+                <img src={review.album_image} alt={review.album_name} />
+              </div>
+              <div className={styles.reviewContainer}>
+                <h3 className={styles.headline}>{review.headline}</h3>
+                <p className={styles.author}>By: {review.username}</p>
+                <p className={styles.review}>On: {review.updated_at}</p>
+                <p className={styles.review}>{review.review}</p>
+                <p className={styles.rating}>Rating: {review.rating} / 5</p>
+
+                <p className={styles.favorite}>
+                  {review.favorite ? "Favorite: ❤️" : ""}
+                </p>
+                <Link
+                  className={styles.linkToAlbum}
+                  to={`/album/${review.album_spotify_id}`}>
+                  View Album
+                </Link>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>No reviews yet.</p>
+        )}
+      </div>
     </div>
   );
 };
