43baf73a4eb1587fbe24d5b68c3726a9496fc147
Scott Kingsley <scott.kingsley@kingsleyimages.comgh auth logingit config --global user.email scott.kingsley@kingsleyimages.co>
2025-03-24 15:35:41 -0600

    search initial commit

--- capstonFE/src/components/Navbar.jsx
+++ capstonFE/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import jukeboxdLogo from "../images/jukeboxdLogoTransparent.png";
+import App from "./Search";
 
 function Navbar() {
   const navigate = useNavigate();
